d27f190cd7fd7c884b42a7b87d3a6d91081d1f37
Kezia Campos <kezia.campos@icloud.com>
2023-05-12 12:22:19 -0300

    atualização

--- js/desenho/desenho.js
+++ js/desenho/desenho.js
@@ -2,25 +2,71 @@ const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 
+const canvasQuadro = document.getElementById('quadro');
+
+var ctx = canvasQuadro.getContext('2d');
+var desenhando = false;
+var x, y;
+
+function drawHandLandmarks(results, ctx) {
+  if (results.multiHandLandmarks) {
+    for (const landmarks of results.multiHandLandmarks) {
+      const landmark = landmarks[4]; // seleciona a ponta do dedo indicador (landmark 4)
+      ctx.beginPath();
+      ctx.arc(landmark.x * canvasQuadro.width, landmark.y * canvasQuadro.height, 5, 0, 2 * Math.PI);
+      ctx.fillStyle = '#1D3557';
+      ctx.fill();
+    }
+  }
+}
+
+function limpar() {
+  var canvas = document.getElementById("quadro");
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 // Função que processa os resultados da detecção de mãos
 function onResults(results)
 {
-    canvasCtx.save();
-    // Limpa o canvas
-    canvasCtx.clearRect( 0, 0, canvasElement.width, canvasElement.height );
+  canvasCtx.save();
+  // Limpa o canvas
 
-    // Desenha a imagem da câmera no canvas
-    canvasCtx.drawImage( results.image, 0, 0, canvasElement.width, canvasElement.height );
-    if (results.multiHandLandmarks)
+  // Desenha a imagem da câmera no canvas
+  canvasCtx.drawImage( results.image, 0, 0, canvasElement.width, canvasElement.height );
+  if (results.multiHandLandmarks)
     {
-        for (const landmarks of results.multiHandLandmarks)
-        {
-            //drawLandmarks(canvasCtx, landmarks, {color: '#E63946', lineWidth: 1});
-            drawLandmarks(canvasCtx, [landmarks[8]], {color: '#E63946', lineWidth: 1});
-        }
+      for (const landmarks of results.multiHandLandmarks)
+      {
+        drawLandmarks(canvasCtx, [landmarks[8]], {color: '#E63946', lineWidth: 1});
+      }
     }
-    canvasCtx.restore();
+  // Desenha as landmarks das mãos no canvasQuadro
+  drawHandLandmarks(results, canvasQuadro.getContext('2d'));
+  
+  canvasCtx.restore();
 }
+
+canvasQuadro.addEventListener("mouseup", function(evt) {
+  desenhando = true;
+  x = evt.clientX - canvasQuadro.getBoundingClientRect().left;
+  y = evt.clientY - canvasQuadro.getBoundingClientRect().top;
+});
+
+canvasQuadro.addEventListener("mousemove", function(evt) {
+  if (desenhando) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    x = evt.clientX - canvasQuadro.getBoundingClientRect().left;
+    y = evt.clientY - canvasQuadro.getBoundingClientRect().top;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+});
+
+
+
+
 
 // Configura o detector de mãos do MediaPipe Hands
 const hands = new Hands({locateFile: (file) => {
@@ -31,8 +77,8 @@ hands.setOptions({
   selfieMode: true,
   maxNumHands: 1,
   modelComplexity: 1,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
+  minDetectionConfidence: 0.9,
+  minTrackingConfidence: 0.9
 });
 hands.onResults(onResults);
 
@@ -46,5 +92,3 @@ const camera = new Camera(videoElement,
   height: 768
 });
 camera.start();
-
-observer.observe(document.body, { childList: true, subtree: true });
